eb43b8600b9bb50470cfb21a4f4bc8750a98f3bd
pavithra <pavithrarpee12@gmail.com>
2022-06-01 13:18:05 +0530

    2nd commit

--- src/main/java/com/ToolsQA/ObjectRepository/ToolsQAPage.java
+++ src/main/java/com/ToolsQA/ObjectRepository/ToolsQAPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//toot QA page
 public class ToolsQAPage {
 
 	@FindBy(xpath="//h5[.='Elements']/../..//div[@class='avatar mx-auto white']")
