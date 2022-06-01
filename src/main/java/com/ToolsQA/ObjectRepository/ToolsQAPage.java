492e160d407e2a1ae1b9db933fd43997606e5f98
pavithra <pavithrarpee12@gmail.com>
2022-06-01 17:01:22 +0530

    ToolQA

--- src/main/java/com/ToolsQA/ObjectRepository/ToolsQAPage.java
+++ src/main/java/com/ToolsQA/ObjectRepository/ToolsQAPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//ToolsQAPage
 public class ToolsQAPage {
 
 	@FindBy(xpath="//h5[.='Elements']/../..//div[@class='avatar mx-auto white']")
